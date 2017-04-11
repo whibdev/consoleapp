f492ba5f4cf1e34457215516b36f3170a3551300
Mike Whibley <mike.whibley@gmail.com>
2017-04-11 16:37:58 -0700

    added chester class

--- App/Program.cs
+++ App/Program.cs
@@ -7,6 +7,8 @@ namespace App
         static void Main(string[] args)
         {
             Console.WriteLine("You've been hacked!");
+            var chester = new Chester();
+            Console.WriteLine(chester.Dob.ToString("yy-MMM-dd"));
         }
     }
 }
