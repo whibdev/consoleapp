f90fb472854ff1374c776ef9aac8e78842a1a379
Mike Whibley <mike.whibley@gmail.com>
2017-04-11 16:51:34 -0700

    deleted files

--- App/Program.cs
+++ App/Program.cs
@@ -6,9 +6,7 @@ namespace App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You've been hacked!");
-            var chester = new Chester();
-            Console.WriteLine(chester.Dob.ToString("yy-MMM-dd"));
+            Console.WriteLine("You've got reverted copy!");
         }
     }
 }
